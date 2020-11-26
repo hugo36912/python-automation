9b4f2f6e8b1e25710af608d3d2763aaf5973ec1c
hugo369 <ken2007123@gmail.com>
2020-11-26 16:29:59 +0800

    Add files via upload

--- getConfig_SRX.py
+++ getConfig_SRX.py
@@ -6,52 +6,33 @@ class getConfig_SRX(object):
     def startGetConfig(deviceName, excelDeviceFull):
        
         DeviceConfig = []
-        #excel_get_data = []
-        #excel_get_data = getDataFromExcel.startGetData("Path")       
-        #inputData = []    
-        #for rowData in excel_get_data:
-        #    inputData.append(rowData[1])
-            
-            
+                       
         DestPath = excelDeviceFull[0] + "\\" + excelDeviceFull[2] + "\\" + deviceName
-        tmpDestPath = excelDeviceFull[0] + "\\" + excelDeviceFull[3] + "\\"  + deviceName
         
         deviceFullPathName = DestPath
-        deviceTmpFullPathName = tmpDestPath
                
-        #deviceFullPathName = "D:\\PythonFirewall_PUB\\PUBv1\\tmpFirewallConfig\\tmpConfig\\" + deviceName
-        #deviceTmpFullPathName = "D:\\PythonFirewall_PUB\\PUBv1\\tmpFirewallConfig\\tmpConfig\\tmp" + deviceName
-
-        File=open(deviceTmpFullPathName, "r")        
-        if File.mode == 'r':           
-            contents = File.readlines()
-            for curReadStr in contents:
-            #print(contents)
-                if "from-zone" in curReadStr and "to-zone" in curReadStr:
-                    DeviceConfig.append(curReadStr)
-                if "address-book" in curReadStr and "address-set" in curReadStr:
-                    DeviceConfig.append(curReadStr)
-                if "address-book" in curReadStr and "address" in curReadStr:   
-                    DeviceConfig.append(curReadStr)
-                if "set applications application" in curReadStr:
-                    DeviceConfig.append(curReadStr)
-                    #pass
-                    
         FileTmp=open(deviceFullPathName, "r")        
         if FileTmp.mode == 'r':           
             contents = FileTmp.readlines()
             for curReadStr in contents:
             #print(contents)
+                if "#" in curReadStr:
+                    continue
+                if "delete" in curReadStr and "schedulers" not in curReadStr :
+                    hj=curReadStr
+                    hj=hj.replace("delete","set")
+                    if hj in DeviceConfig:
+                     DeviceConfig.remove(hj)
+                     continue
                 if "from-zone" in curReadStr and "to-zone" in curReadStr:
                     DeviceConfig.append(curReadStr)
                 if "address-book" in curReadStr and "address-set" in curReadStr:
                     DeviceConfig.append(curReadStr)
-                if "address-book" in curReadStr and "address" in curReadStr:   
+                if "address-book" in curReadStr and "address-set" not in curReadStr:   
                     DeviceConfig.append(curReadStr)
                 if "set applications application" in curReadStr:
                     DeviceConfig.append(curReadStr) 
-
-         
+        #print(DeviceConfig[-1])
         return DeviceConfig   
         #print(DeviceConfig)
 
